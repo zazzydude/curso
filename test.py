bfffcc9d5aec372db78c3b68c63b9e9631d6d880
NaryOgrady <jluis.garza@live.com.mx>
2017-06-06 21:23:30 -0500

    add test file

---
+++ test.py
@@ -0,0 +1,10 @@
+names = ['jose', 'luis', 'tere', 'marecela']
+
+og_name = 'luis'
+
+for name in names:
+    if og_name == name:
+        print('the name is in there')
+        break
+    else:
+        print('name not found')
